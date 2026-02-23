b0b33359ea990fd5e0e0eab0e5c9751124b3c331
abhishek.kale <abhisahebpatil@gmail.com>
2026-02-23 16:34:28 +0530

    completed-ng-template

--- src/app/component/angular-view-and-template/ng-template/ng-template.ts
+++ src/app/component/angular-view-and-template/ng-template/ng-template.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { NgTemplateOutlet, NgIf } from '@angular/common';
+import { Component, signal, WritableSignal } from '@angular/core';
 
 @Component({
   selector: 'app-ng-template',
-  imports: [],
+  imports: [NgTemplateOutlet, NgIf],
   templateUrl: './ng-template.html',
   styleUrl: './ng-template.css',
 })
 export class NgTemplate {
+   isLogginPage:WritableSignal<boolean> = signal(true);
 
+   isAdmin:WritableSignal<boolean> = signal(true);
+
+   onclick(){
+      this.isLogginPage.update((current)=> !current);
+   }
+
+   onChangeBlock(){
+    this.isAdmin.set(false);
+   }
 }
